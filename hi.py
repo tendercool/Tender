25e4bd9616b8cad5687b86af7593b520ffaf190e
tendercool <93910468@qq.com>
2021-01-03 22:14:31 +0800

    tendercool

--- hi.py
+++ hi.py
@@ -1,1 +1,10 @@
-print('gogogo')
+import sys
+from PyQt5.QtWidgets import *
+from Pyqt5.Qt import *
+import random
+
+class My_window(QMainWindow):
+    def __init__():
+        super().__init__()
+
+        
